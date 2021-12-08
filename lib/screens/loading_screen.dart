5c31bb1a4db930b83fbc4d261cbaf7dcf239519a
Hamza Zahid <hamzazahid2012@gmail.com>
2021-12-08 08:40:43 +0500

    .

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clima_weather/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import '../key.dart';
 
@@ -12,14 +13,16 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? latitude;
+  double? longitude;
+  http.Response? response;
   void getLocation() async {
     await _locationService.getCurrentLocation();
-    print(latitude);
+    latitude = _locationService.latitude ?? 0.0;
+    longitude = _locationService.longitude ?? 0.0;
     print(longitude);
+    print(latitude);
   }
-
-  double get latitude => _locationService.latitude ?? 0.0;
-  double get longitude => _locationService.longitude ?? 0.0;
 
   final LocationService _locationService = LocationService();
   @override
@@ -30,9 +33,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getData() async {
     Uri uri = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?$latitude=35&$longitude=139&appid=$apiKey');
-    Response response = await get(uri);
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    response = await http.get(uri);
+    if (response!.statusCode == 200) {
+      String data = response!.body;
+      final decodedData = jsonDecode(data);
+      int id = decodedData['weather'][0]['id'];
+      double temperature = decodedData['main']['temp'];
+      String city = decodedData['name'];
+      print("$city, $temperature, $id");
+    } else {
+      print('Error: Couldn\'t get data for this location');
+    }
   }
+
+  void unpack() {}
 
   @override
   Widget build(BuildContext context) {
@@ -40,9 +55,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            _locationService.getCurrentLocation();
-            print(_locationService.latitude);
-            print(_locationService.longitude);
+            getData();
           },
           child: const Text('Get Location'),
         ),
